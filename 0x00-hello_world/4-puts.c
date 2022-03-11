1b87c3a2f9dd51a7df3dad510c8e97cf99b28abd
Professorpreneur <daviduche809@yahoo.com>
2022-03-11 12:59:46 -0800

    safe

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * "programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
-int main (void)
+int main(void)
 {
 puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 }
